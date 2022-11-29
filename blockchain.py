77652a109c6554057c39f16aa577c9026071de59
kangcshin <kangcshin@gmail.com>
2022-11-29 01:30:36 +0000

    Nov 28, 2022, 5:30 PM

--- blockchain.py
+++ blockchain.py
@@ -95,7 +95,6 @@ class Blockchain(object):
         :param proof: <int> Current Proof
         :return: <bool> True if correct, False if not.
         """
-
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
